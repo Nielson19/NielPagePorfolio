c007be338f2e7832d42202a1eee47ba295700f10
Niel <danielescobar492@gmail.com>
2024-06-11 17:03:33 -0400

    Added Music Section
    Added Insipido and Agridulce in the music section

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,13 +6,13 @@ export default function HeaderNav() {
         <nav className="navbar">
             <ul className="navbar-nav">
                 <li className="nav-item">
-                    <button className="nav_button">Home</button>
+                    <button className="nav_button">Socials</button>
                 </li>
                 <li className="nav-item">
                     <button className="nav_button">Music</button>
                 </li>
                 <li className="nav-item">
-                    <button className="nav_button">About</button>
+                    <button className="nav_button">Coding</button>
                 </li>
             </ul>
         </nav>
--- src/Music.js
+++ src/Music.js
@@ -1,31 +1,38 @@
 import React from 'react';
 import './Music.css';
-import insipidGif from './Media/Gifs/Insipido.gif';
+import insipidoImg from './Media/Photos/isaContainer.PNG';
+import agridulceImg from './Media/Photos/medicensantoContainer.png';
+import insipidoGif from './Media/Gifs/Insipido.gif';
+import agridulceGif from './Media/Gifs/Agridulce.gif'
 
 function MusicCard(props) {
-    const backgroundColor = props.backgroundColor, title = props.title,
+    const backgroundColor = props.backgroundColor, backgroundImage = props.backgroundImage, title = props.title,
         artist = props.artist, url = props.url, image = props.image;
     return(
-        <div className="musicCardContainer" style={{
-            "backgroundColor": backgroundColor,
-        }}>
+        <div className="musicCardContainer">
 
             <div className="imageContainer">
                 <img src={image}/>
             </div>
 
-            <div className="textContainer">
+            <div className="textContainer"
+                 style={{
+                     // backgroundImage should be size = 2048 x 858 px
+                     backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url(${backgroundImage})`,
+                     backgroundSize: 'cover',
+                     backgroundRepeat: 'no-repeat',
+                     backgroundPosition: 'center',
+                 }}>
                 <h1>{title}</h1>
                 <p>{artist}</p>
-            </div>
 
-            <iframe className="Player" style={{borderRadius :12}}
-                    src={url}
-                    frameBorder="0"
-                    width="600" height="352"
-                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-                    loading="lazy">
-            </iframe>
+            </div>
+            {/*<iframe style={{borderRadius: 12 }}*/}
+            {/*        src="https://open.spotify.com/embed/track/1dmlsc55ojHHHuEUWyB6yd?utm_source=generator&theme=0"*/}
+            {/*        width="100%" height="152" frameBorder="0" allowFullScreen=""*/}
+            {/*        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"*/}
+            {/*        loading="lazy"></iframe>*/}
+
         </div>
     )
 }
@@ -34,11 +41,9 @@ export default function MusicFunction() {
 
     return (
         <div className="musicContainer">
-            <MusicCard backgroundColor= "lightblue"  title="Insìpido" artist="Niel Klopen x Isa" url= "https://open.spotify.com/embed/track/1dmlsc55ojHHHuEUWyB6yd?utm_source=generator&theme=0" image = {insipidGif}  />
-            <MusicCard backgroundColor="lightblue" title="Insìpido" artist="Niel Klopen x Isa" url= "https://open.spotify.com/embed/track/1dmlsc55ojHHHuEUWyB6yd?utm_source=generator&theme=0" image = {insipidGif}  />
-
-
-
+            <MusicCard backgroundColor="lightblue" title="Insìpido" artist="Niel Klopen x Isa"
+                       url="https://open.spotify.com/embed/track/1dmlsc55ojHHHuEUWyB6yd?utm_source=generator&theme=0" image = {insipidoGif} backgroundImage= {insipidoImg}/>
+            <MusicCard backgroundColor= "lightblue"  title="Agridulce" artist="Niel Klopen x Medicensanto" url= "https://open.spotify.com/embed/track/1dmlsc55ojHHHuEUWyB6yd?utm_source=generator&theme=0" image = {agridulceGif} backgroundImage= {agridulceImg}/>
 
         </div>
     )
